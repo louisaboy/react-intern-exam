7f2d14fcb0579a014c631c0a2c0eb11687822517
louisaboy <l.a.aboy24@gmail.com>
2023-04-18 13:30:07 +0800

    added files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Imported required packages
 require('dotenv').config({ path: '../.env.sample' });
 const port = process.env.PORT || 3001;
+const path = require("path");
 
 const express = require("express");
 const cors = require("cors");
@@ -18,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-const mongoDatabase = process.env.mongoDatabase
+const mongoDatabase = process.env.MONGO_URI
 
 mongoose.connect(mongoDatabase).catch(err => console.log(err));
 // const invoiceSchema = mongoose.Schema({
@@ -84,7 +85,14 @@ var route = require('./routes/routes')
 
 app.use("/", route);
 
-app.listen(port, function () {
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("src/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    }) 
+}
+
+app.listen(process.env.PORT || 3001, function () {
     console.log(`Server is running on port: ${port}`);
     console.log(`Connected Database: ${mongoDatabase}`);
 });
